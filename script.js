6872f3ba9d32ee3cd8dc587e6d83ce95a853a2a8
Muhammad Rafie <79514755+muraft@users.noreply.github.com>
2023-06-11 21:05:03 +0700

    Sistem ngambil ayat acak (blom jadi)

--- script.js
+++ script.js
@@ -7,25 +7,24 @@ fetch('https://equran.id/api/surat').then(res=>res.json()).then(data=>{
 
 async function mulai(){
   //Dapetin nilai dari inputan, sementara defaultnya Al fatihah 1-7
-  const surat1=document.getElementById('select1').value;
-  const ayat1=document.getElementById('ayat1').value||1;
-  const surat2=document.getElementById('select2').value||1;
-  const ayat2=document.getElementById('ayat2').value||7;
+  let surat1=document.getElementById('select1').value;
+  let ayat1=document.getElementById('ayat1').value||1;
+  let surat2=document.getElementById('select2').value||1;
+  let ayat2=document.getElementById('ayat2').value||7;
   
   //Ngecek ayat pertama yang dimasukin ada di suratnya apa enggak
-  let res=await fetch('https://equran.id/api/surat/'+surat1);
-  const ayatSurat1=(await res.json()) .jumlah_ayat;
-  console.log(ayatSurat1);
+  let ayatSurat1=(await(await fetch('https://equran.id/api/surat/'+surat1)).json()).jumlah_ayat;
   if(ayat1<=0 || ayat1>ayatSurat1){
     return alert('Ayat tidak ditemukan');
   }
  
  //Ngecek ayat kedua yang dimasukin ada di suratnya apa enggak
-  res=await fetch('https://equran.id/api/surat/'+surat2);
-  const ayatSurat2=(await res.json()) .jumlah_ayat;
+  let ayatSurat2=(await(await fetch('https://equran.id/api/surat/'+surat2)).json()).jumlah_ayat;
   if(ayat2<=0 || ayat2>ayatSurat2){
     return alert('Ayat tidak ditemukan');
   }
+  
+  if(surat1>surat2)return alert('Surat yang lebih awal harus ada di pertama');
   
   //Pindah halaman
   location.href=`/mulai.html?surat1=${surat1}&ayat1=${ayat1}&surat2=${surat2}&ayat2=${ayat2}`;
